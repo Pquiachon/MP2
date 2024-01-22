e56ec141be3e4d02c60937a02959389d29aa7160
Pquiachon <johnpaulquiachon2904@gmail.com>
2024-01-22 16:58:57 +0800

    adding final retouch

--- FINAL MP2/js/index.js
+++ FINAL MP2/js/index.js
@@ -1,49 +1,26 @@
 $(document).ready(function () {
-  $("#form_bmi").submit(function (x) {
-    x.preventDefault();
-
-    let height = Number($("#height_bmi").val());
-    let weight = Number($("#weight_bmi").val());
-
-    let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
-    if (bmi < 16) {
-      $("#bmi_status").text("Severe Thinness.");
-      $("#bmi_status").css("color", "red");
-      $("#bmi_result").css("color", "red");
-    } else if (bmi >= 16 && bmi < 17) {
-      $("#bmi_status").text("Moderate Thinness.");
-      $("#bmi_status").css("color", "orange");
-      $("#bmi_result").css("color", "orange");
-    } else if (bmi >= 17 && bmi < 18.5) {
-      $("#bmi_status").text("Mild Thinnes.");
-      $("#bmi_status").css("color", "yellow");
-      $("#bmi_result").css("color", "yellow");
-    } else if (bmi >= 18.5 && bmi < 25) {
-      $("#bmi_status").text("Normal.");
-      $("#bmi_status").css("color", "blue");
-      $("#bmi_result").css("color", "blue");
-    } else if (bmi >= 25 && bmi < 30) {
-      $("#bmi_status").text("Overweight.");
-      $("#bmi_status").css("color", "green");
-      $("#bmi_result").css("color", "green");
-    } else if (bmi >= 30 && bmi <= 35) {
-      $("#bmi_status").text("Obese Class-1.");
-      $("#bmi_status").css("color", "yellow");
-      $("#bmi_result").css("color", "yellow");
-    } else if (bmi >= 35 && bmi < 40) {
-      $("#bmi_status").text("Obese Class-2.");
-      $("#bmi_status").css("color", "orange");
-      $("#bmi_result").css("color", "orange");
-    } else if (bmi > 40) {
-      $("#bmi_status").text("Obese Class-3.");
-      $("#bmi_status").css("color", "red");
-      $("#bmi_result").css("color", "red");
+  if (localStorage.getItem("check_input") != null) {
+    if (localStorage.getItem("check_input") == 1) {
+      window.location.href = "home.html";
     }
-
-    $("#bmi_result").text(bmi);
-  });
-  $("#reset_button").click(function () {
-    $("#bmi_status").text("");
-    $("#bmi_result").text("");
+  }
+  $("#form_content").submit(function (x) {
+    let email = $("#email_input").val();
+    let pw = $("#password_input").val();
+    let check_input = $("#check_input").prop("checked");
+    if (email == "user@gmail.com") {
+      if (pw == "123") {
+        if (check_input) {
+          localStorage.setItem("check_input", 1);
+        }
+        localStorage.setItem("logged_in", 1);
+        window.location.href = "home.html";
+      } else {
+        alert("ERROR: Invalid password!");
+      }
+    } else {
+      alert("ERROR: Invalid email!");
+    }
+    x.preventDefault();
   });
 });
